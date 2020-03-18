95991402561a85396b58808a49dbb942c6040e87
suhailmir <suhabb@gmail.com>
2020-03-18 12:31:36 +0000

    Inital Setup

---
+++ src/main/java/com/nemisis/nemisis/NemisisApplication.java
@@ -0,0 +1,13 @@
+package com.nemisis.nemisis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NemisisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NemisisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nemisis/nemisis/NemisisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nemisis.nemisis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NemisisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
